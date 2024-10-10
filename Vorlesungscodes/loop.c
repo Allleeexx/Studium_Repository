1575f24f4768400e57d0122baeab0bef8074c6fa
Some Student. Put your name here <your.email@your.provider>
2024-10-10 16:14:05 +0200

    Test

---
+++ Vorlesungscodes/loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int add(int a, int b){
+	return a+b;
+}
+
+int main(){
+	int i,j;
+	for(i=0; i<10; i++){
+		j=i+10;
+		printf("i=%d j=%d i+j=%d\n",i,j,add(i,j));
+	}
+	return 0;
+}
