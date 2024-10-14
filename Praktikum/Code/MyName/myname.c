2097331e13a9fb8e759038a7d9405fffe9526d13
Alex <alexanderroth735@icloud.com>
2024-10-14 13:57:05 +0200

    Latest update VS code

--- Praktikum/Code/MyName/myname.c
+++ Praktikum/Code/MyName/myname.c
@@ -4,10 +4,10 @@ int main(){
 	char name[] = "Alex";
 	int day = 25;
 	int month = 06;
-	int year = 2002; 
+	int year = 2002;
 
 	printf("Mein Name ist %s\n", name);
 	printf("Mein Geburtsdatum ist der %02d.%02d.%04d\n", day, month, year);
+	printf("Das hier ist ein Test. Das hier habe ich auf meinem PC in Visual Studio Code programmiert\n");
 	return 0;
 }
-
