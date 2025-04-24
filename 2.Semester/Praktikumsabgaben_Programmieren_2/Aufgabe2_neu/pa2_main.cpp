5f3ac219e5a618e405c5d6dc30873f5d958ade9a
Roth <alr9045@thi.de>
2025-04-24 12:38:27 +0200

    2 wirklich fertig

--- 2.Semester/Praktikumsabgaben_Programmieren_2/Aufgabe2_neu/pa2_main.cpp
+++ 2.Semester/Praktikumsabgaben_Programmieren_2/Aufgabe2_neu/pa2_main.cpp
@@ -15,11 +15,25 @@ int main(){
     //Ballistic ball("1", 20.0, 20.0);
     Ballistic ball = new Ballistic("1", 20.0, 20.0);
     
+    vector<Ufo*> ufos {vert, ball};
+
 
 
     cout <<"Triff deine Auswahl: \nVertical ----> 0\nBallistic ----> 1" << "\n";
     cin >> usereingabe;
 
+    if(usereingabe == 0 || usereingabe == 1){
+        if(usereingabe == 0){            //Dann mit Vert
+            ufos[0].flyToDest(5.0, -1.5, 4.0, 10);
+            ufos[0].flyToDest(-3.0, 0.0, 8.0, 5);
+        }else{
+            ufos[1].flyToDest(5.0, -1.5, 4.0, 10);
+            ufos[1].flyToDest(-3.0, 0.0, 8.0, 5);
+        }
+    }else{
+        cout << "Falsche Eingabe";
+    }
+/*
     switch (usereingabe)
     {
     case 0:
@@ -34,15 +48,16 @@ int main(){
     case 1:
         // fly from (0.0, 0.0, 0.0) to (5.0, -1.5, 0.0)
         // at altitude 4.0 with 10 km/h
-        vert.flyToDest(5.0, -1.5, 4.0, 10);
+        ball.flyToDest(5.0, -1.5, 4.0, 10);
 
         // fly from (5.0, -1.5, 0.0) to (-3.0, 0.0, 0.0)
         // at altitude 8.0 with 5 km/h
-        vert.flyToDest(-3.0, 0.0, 8.0, 5);
+        ball.flyToDest(-3.0, 0.0, 8.0, 5);
         break;
     default:
         break;
     }
-    
+*/
+
     return 0;
 }
