7ae5f2158385dc6a7040d57a393afe434346ddf8
Alex <alexanderroth735@icloud.com>
2025-04-26 15:28:12 +0200

    Aufgabe 2 fertig

--- 2.Semester/Praktikumsabgaben_Programmieren_2/Aufgabe2_neu/pa2_main.cpp
+++ 2.Semester/Praktikumsabgaben_Programmieren_2/Aufgabe2_neu/pa2_main.cpp
@@ -11,9 +11,9 @@ int main(){
     int usereingabe;
 
     //Vertical vert("1");
-    Vertical vert = new Vertical("1");
+    Vertical* vert = new Vertical("1");
     //Ballistic ball("1", 20.0, 20.0);
-    Ballistic ball = new Ballistic("1", 20.0, 20.0);
+    Ballistic* ball = new Ballistic("1", 20.0, 20.0);
     
     vector<Ufo*> ufos {vert, ball};
 
@@ -22,43 +22,33 @@ int main(){
     cout <<"Triff deine Auswahl: \nVertical ----> 0\nBallistic ----> 1" << "\n";
     cin >> usereingabe;
 
-        if(usereingabe == 0 || usereingabe == 1){
-            if(usereingabe == 0){            //Dann mit Vert
-                ufos[0].flyToDest(5.0, -1.5, 4.0, 10);
-                ufos[0].flyToDest(-3.0, 0.0, 8.0, 5);
-            }else{
-                ufos[1].flyToDest(5.0, -1.5, 4.0, 10);
-                ufos[1].flyToDest(-3.0, 0.0, 8.0, 5);
-            }
-        }else{
-            cout << "Falsche Eingabe";
-        }
-    
-/*
-    switch (usereingabe)
-    {
+    switch (usereingabe){
     case 0:
         // fly from (0.0, 0.0, 0.0) to (5.0, -1.5, 0.0)
         // at altitude 4.0 with 10 km/h
-        vert.flyToDest(5.0, -1.5, 4.0, 10);
+        ufos[0]->flyToDest(5.0, -1.5, 4.0, 10);
 
         // fly from (5.0, -1.5, 0.0) to (-3.0, 0.0, 0.0)
         // at altitude 8.0 with 5 km/h
-        vert.flyToDest(-3.0, 0.0, 8.0, 5);
+        ufos[0]->flyToDest(-3.0, 0.0, 8.0, 5);
         break;
     case 1:
         // fly from (0.0, 0.0, 0.0) to (5.0, -1.5, 0.0)
         // at altitude 4.0 with 10 km/h
-        ball.flyToDest(5.0, -1.5, 4.0, 10);
+        ufos[1]->flyToDest(5.0, -1.5, 4.0, 10);
 
         // fly from (5.0, -1.5, 0.0) to (-3.0, 0.0, 0.0)
         // at altitude 8.0 with 5 km/h
-        ball.flyToDest(-3.0, 0.0, 8.0, 5);
+        ufos[1]->flyToDest(-3.0, 0.0, 8.0, 5);
         break;
     default:
+        cout << "Falsche Eingabe \n";
         break;
     }
-*/
+
+
+    delete vert;
+    delete ball;
 
     return 0;
 }
