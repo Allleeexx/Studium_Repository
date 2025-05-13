64826a85e82bbc8c2e40e3667ff67d7ee6aa752e
Roth <alr9045@thi.de>
2025-05-13 11:11:02 +0200

    Aufgabe 1 Betriebssysteme

---
+++ 2.Semester/Praktikum_Informatik_2/Betriebssysteme/Aufgabe1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void *example_fct(void *args){
+	size_t i;
+	struct timespec sleep = {0, 1000};
+
+	for(i =0; i<100; i++){
+		putchar('a' +  pthread_self() % 26);
+		nanosleep(&sleep, NULL);
+	}
+	return NULL;
+}
+
+int main(){
+	pthread_t threadA, threadB;
+	pthread_create(&threadA, NULL, &example_fct, NULL);
+	pthread_create(&threadB, NULL, &example_fct, NULL);
+
+	pthread_join(threadA, NULL);
+	pthread_join(threadB, NULL);
+}
