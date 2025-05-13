997ef41bb1c6ac426761d5fbdd924d1134c70b74
Some Student. Put your name here <your.email@your.provider>
2025-05-13 11:47:17 +0200

    Betriebssysteme Aufgabe 1 fertig

--- 2.Semester/Praktikum_Informatik_2/Betriebssysteme/Aufgabe1.c
+++ 2.Semester/Praktikum_Informatik_2/Betriebssysteme/Aufgabe1.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <string.h>
+
+typedef struct{
+	char name [100];
+	int length;	
+}Student;
+
 
 void *example_fct(void *args){
-	size_t i;
-	struct timespec sleep = {0, 1000};
-
-	for(i =0; i<100; i++){
-		putchar('a' +  pthread_self() % 26);
-		nanosleep(&sleep, NULL);
-	}
+	Student *student = (Student*) args;
+	student->length = strlen(student->name);
+	printf("Name in example: %s\n", student->name);
+	printf("Length von Name: %d\n\n", student->length);
 	return NULL;
 }
 
-int main(){
-	pthread_t threadA, threadB;
-	pthread_create(&threadA, NULL, &example_fct, NULL);
-	pthread_create(&threadB, NULL, &example_fct, NULL);
 
+
+int main(){
+	Student student;	
+	strncpy(student.name, "Alex", 100);
+	pthread_t threadA/*, threadB*/;
+	pthread_create(&threadA, NULL, &example_fct, &student);
+	//pthread_create(&threadB, NULL, &example_fct, NULL);
+	
 	pthread_join(threadA, NULL);
-	pthread_join(threadB, NULL);
+//	pthread_join(threadB, NULL);
+
+	printf("Name in main: %s\n", student.name);
+	printf("Length in main: %d\n", student.length);
 }
