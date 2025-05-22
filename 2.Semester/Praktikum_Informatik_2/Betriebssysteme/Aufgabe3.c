df0b7116f84083d515d1a4e15af80763ed8d91cf
Alex <alexanderroth735@icloud.com>
2025-05-22 11:30:08 +0200

    Versuch 1 3. Aufgabe

--- 2.Semester/Praktikum_Informatik_2/Betriebssysteme/Aufgabe3.c
+++ 2.Semester/Praktikum_Informatik_2/Betriebssysteme/Aufgabe3.c
@@ -7,8 +7,7 @@
 #define RANGE_END 100000000
 #define THREAD_COUNT 16
 
-
-typedef struct{
+typedef struct {
 	int start;
 	int end;
 	int maxStartValue;
@@ -16,103 +15,97 @@ typedef struct{
 	struct timespec start_time;
 	struct timespec end_time;
 	float totalRuntime;
-}Rechner;
+} Rechner;
 
-typedef struct {
- 	time_t tv_sec; /* Sekunden */
- 	long tv_nsec; /* Nanosekunden */
-}timespec;
-
+pthread_mutex_t mutex;
+int globalMaxIterations = 0;
+int globalMaxStartValue = 0;
 
 int collatzfunction(unsigned long long x){
 	int cnt = 0;
-	while(x>1){
+	while(x > 1){
 		if(x % 2 == 0){
-			x = x/2;
+			x = x / 2;
 		}else{
-			x = 3*x+1;
+			x = 3 * x + 1;
 		}
-		cnt = cnt + 1;
+		cnt++;
 	}
 	return cnt;
 }
 
-void* threadFunction(void * arg){
+void* threadFunction(void* arg){
 	Rechner* r = (Rechner*) arg;
 
 	clock_gettime(CLOCK_MONOTONIC, &r->start_time);  // Startzeit
 
-	int maxIterations = 0;
-	int maxStartValue = 0;
+	int localMaxIterations = 0;
+	int localMaxStartValue = 0;
 
-	for(int i=r->start ; i<=r->end; i++){
+	for(int i = r->start; i <= r->end; i++){
 		int iterations = collatzfunction((unsigned long long)i);
-		if(iterations > maxIterations){
-			maxIterations = iterations;
-			maxStartValue = i;
+		if(iterations > localMaxIterations){
+			localMaxIterations = iterations;
+			localMaxStartValue = i;
 		}
 	}
 
-	r->maxIterations = maxIterations;
-	r->maxStartValue = maxStartValue;
+	r->maxIterations = localMaxIterations;
+	r->maxStartValue = localMaxStartValue;
 
 	clock_gettime(CLOCK_MONOTONIC, &r->end_time);  // Endzeit
+	r->totalRuntime = (r->end_time.tv_sec - r->start_time.tv_sec)
+	                + (r->end_time.tv_nsec - r->start_time.tv_nsec) / 1e9;
 
-	r->totalRuntime = (r->end_time.tv_sec - r->start_time.tv_sec) + (r->end_time.tv_nsec - r->start_time.tv_nsec) / 1e9;
-
+	// --- MUTEX-geschützter Zugriff auf globale Werte ---
+	pthread_mutex_lock(&mutex);
+	if(localMaxIterations > globalMaxIterations){
+		globalMaxIterations = localMaxIterations;
+		globalMaxStartValue = localMaxStartValue;
+	}
+	pthread_mutex_unlock(&mutex);
 
 	pthread_exit(NULL);
 }
 
 int main(){
-	
 	pthread_t threads[THREAD_COUNT];
 	Rechner bereiche[THREAD_COUNT];
 
-
 	int block_size = RANGE_END / THREAD_COUNT;
-	
+
+	// Mutex initialisieren
+	pthread_mutex_init(&mutex, NULL);
+
 	struct timespec start, end;
 	clock_gettime(CLOCK_MONOTONIC, &start);
 
-
-	for(int i=0; i<THREAD_COUNT; i++){
-		bereiche[i].start = i*block_size +1;
-		bereiche[i].end = (i+1) * block_size;
+	for(int i = 0; i < THREAD_COUNT; i++){
+		bereiche[i].start = i * block_size + 1;
+		bereiche[i].end = (i + 1) * block_size;
 
 		pthread_create(&threads[i], NULL, threadFunction, &bereiche[i]);
 	}	
 
-	//Alle Threads schließen, ansosnten irgnedwelche Werte
-	for(int i=0; i<THREAD_COUNT; i++){
+	for(int i = 0; i < THREAD_COUNT; i++){
 		pthread_join(threads[i], NULL);
 	}
 
 	clock_gettime(CLOCK_MONOTONIC, &end);
 
-
-	//Hier der Bereich um ergebnisse zu printen
-	for(int i=0; i<THREAD_COUNT; i++){
-		printf("Bereich %d: \nStartwert: %d\nEndwert: %d\nMaxIterations: %d\nMaxStartValue: %d\nThreadLaufzeit: %lf Sekunden", i, bereiche[i].start, bereiche[i].end, bereiche[i].maxIterations, bereiche[i].maxStartValue, bereiche[i].totalRuntime);
-		printf("\n\n");
-	}
-
-	// Globales Maximum finden
-	int globalMaxIterations = 0;
-	int globalMaxStartValue = 0;
-
 	for(int i = 0; i < THREAD_COUNT; i++){
-		if(bereiche[i].maxIterations > globalMaxIterations){
-			globalMaxIterations = bereiche[i].maxIterations;
-			globalMaxStartValue = bereiche[i].maxStartValue;
-		}
+		printf("Bereich %d:\nStartwert: %d\nEndwert: %d\nMaxIterations: %d\nMaxStartValue: %d\nThreadLaufzeit: %.6f Sekunden\n\n",
+			i, bereiche[i].start, bereiche[i].end, bereiche[i].maxIterations, bereiche[i].maxStartValue, bereiche[i].totalRuntime);
 	}
 
-	printf("Längste Collatz-Folge im Bereich [%d - %d]:\nStartwert: %d\nAnzahl Schritte: %d\n", RANGE_START, RANGE_END, globalMaxStartValue, globalMaxIterations);
-
+	printf("Längste Collatz-Folge im Bereich [%d - %d]:\nStartwert: %d\nAnzahl Schritte: %d\n",
+		RANGE_START, RANGE_END, globalMaxStartValue, globalMaxIterations);
 
 	double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
-	printf("Bearbeitungszeit: %.6f Sekunden\n", elapsed);
+	printf("Gesamtlaufzeit: %.6f Sekunden\n", elapsed);
+
+	// Mutex zerstören
+	pthread_mutex_destroy(&mutex);
 
 	return 0;
 }
